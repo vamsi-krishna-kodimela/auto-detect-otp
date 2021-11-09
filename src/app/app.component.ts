f92810054c963371caa1e4d37006110761a638d1
R3-V3NG3R <vamsikrishnakdml@gmail.com>
2021-11-09 23:47:47 +0530

    OTP LISTENER

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'auto-detect-otp';
+  data = '';
+  ngOnInit() {}
+  ngAfterViewInit() {
+    if ('OTPCredential' in window) {
+      window.addEventListener('DOMContentLoaded', (e) => {
+        const ac = new AbortController();
+        let options = {
+          otp: { transport: ['sms'] },
+          signal: ac.signal,
+        };
+        navigator.credentials
+          .get(options)
+          .then((otp) => {
+            this.data = JSON.stringify(otp);
+            alert(this.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    } else {
+      alert('WebOTP not supported!.');
+    }
+  }
 }
